86af933b22be1b4620d19f96226dc851c2dc771c
Puneet Arora <puneet.arora@moonraft.com>
2019-07-07 11:45:48 +0530

    host first page2

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from "react-redux";
 import { mapStateToProps, mapDispatchToProps } from "./connector";
+import {Helmet} from 'react-helmet'
 //import { getAllAccountSummaryBalanceData } from "../actions/DashboardData";
 import moment from "moment";
 class Dashboard extends Component {
@@ -18,6 +19,11 @@ class Dashboard extends Component {
 console.log(this.props.newData,"-----")
     return (
       <>
+        <Helmet>
+    <title>Here's the Title!</title>
+    <meta name="description" content="This is what you want to show as the page content in the Google SERP Listing" />
+  </Helmet>
+
        <h1>
 Hiiii react app
        </h1>
